be22696cbdfe504341a8144ad0684683342681cf
chenzhifengla <chen1511@foxmail.com>
2017-06-27 10:44:56 +0800

    添加了C++版新客户端

---
+++ cppClient/main.cpp
@@ -0,0 +1,45 @@
+//
+// Created by yingzi on 2017/6/27.
+//
+
+#include "SerialClient.h"
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <unistd.h>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::ostringstream;
+
+string getEventName() {
+    static double p = 0.7;  // increase事件的概率
+    if (rand() > RAND_MAX * p) return "decrease";
+    else return "increase";
+}
+
+int getValueNum() {
+    return rand() % 60;
+}
+
+int main(int argc, char** argv) {
+    SerialClient serialClient("/dev/ttyS0");
+    serialClient.init();
+
+    while (1) {
+        ostringstream oss;
+        oss << "<event name=\"" << getEventName() << "\" value=\"x = " << getValueNum() << "\"/>";
+        if (!serialClient.sendMessage(oss.str())) {
+            cerr << "Send message " << oss.str() << "failed!" << endl;
+            break;
+        }
+        cout << oss.str() << endl;
+        sleep(1);
+    }
+
+    serialClient.closePort();
+
+    return 0;
+}
